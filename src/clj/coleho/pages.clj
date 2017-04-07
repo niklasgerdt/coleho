2456e5bfe03a154079383a346e62055eeb06b5e3
Niklas Gerdt <niklas.gerdt@zervant.com>
2017-04-07 21:49:26 +0300

    added w3 css

--- src/clj/coleho/pages.clj
+++ src/clj/coleho/pages.clj
@@ -2,12 +2,14 @@
   (:require
     [hiccup
      [page :refer [html5]]
-     [page :refer [include-js]]]))
+     [page :refer [include-js]]
+     [page :refer [include-css]]]))
 
 (defn index [a] 
   (html5
     [:head
-     [:title "Coleho Tools"]]
+     [:title "Coleho Tools"]
+     (include-css "w3.css")]
     [:body
      [:h1 "Coleho Tools"]
      [:div#tool]
